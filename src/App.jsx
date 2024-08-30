c69c849b5ecf6bd2ccf01835ce5abf2e6ef8912e
Vaibhav0x <rajjvaibhavv121@gmail.com>
2024-08-31 02:06:49 +0530

    startup logo changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     
     <Box minH="100vh" bg='#0f0a19' color='gray.500' px={6} py={6}>
-      <Box fontSize='3xl' mb={2} color="blue.300" textAlign={['left','center']}>Code Editor</Box>
+      <Box fontSize='3xl' mb={2} color="blue.300" textAlign={['left','center']}>BurNCode</Box>
       <CodeEditor/>
       <Footer/>
     </Box>
