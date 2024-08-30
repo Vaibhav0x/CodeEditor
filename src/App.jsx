1b6344e1714071e8f6382ce378f4f45bb4b909db
Vaibhav0x <rajjvaibhavv121@gmail.com>
2024-08-31 01:53:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import {Box} from "@chakra-ui/react"
+import CodeEditor from "./components/CodeEditor"
+import Footer from "./components/Footer"
+
+function App() {
+
+  return (
+    
+    <Box minH="100vh" bg='#0f0a19' color='gray.500' px={6} py={6}>
+      <Box fontSize='3xl' mb={2} color="blue.300" textAlign={['left','center']}>Code Editor</Box>
+      <CodeEditor/>
+      <Footer/>
+    </Box>
+  )
+}
+
+export default App
+
+
+/* Chakra-ui for css 
+  Piston Api for code editor
+  https://github.com/nikitadev-yt/react-code-editor-app/blob/main/src/components/Output.jsx
+*/
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div bg="gray.500">
+    </div>
+  )
+}
+
+export default Footer
